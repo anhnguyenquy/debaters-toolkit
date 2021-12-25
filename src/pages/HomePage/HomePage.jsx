7121829c012f3a617aedb4d46a9bf9437e53600c
Anh Nguyen <anhnguyenquy2407@gmail.com>
2021-12-25 22:06:54 +0700

    1.1.2: Added PWA screenshots

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -14,7 +14,7 @@ export const HomePage = () => {
         <title>Debaters' toolkit</title>
         <meta
           name='description'
-          content='Generate a random debate motion, search our database of over 7000 motions, calculate break chances or do debate timekeeping online.'
+          content='Generate a random debate motion, search our database of over 7000 motions, calculate break chances or do debate timekeeping.'
         />
         <link rel='canonical' href='https://www.debaterstoolkit.com/' />
       </Helmet>
