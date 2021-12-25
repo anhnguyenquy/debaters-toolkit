1687f44a20840eaf426cf7d3b43c18aa67e58699
Anh Nguyen <anhnguyenquy2407@gmail.com>
2021-12-25 13:30:48 +0700

    icons update (again)

--- public/electron.js
+++ public/electron.js
@@ -1,7 +1,7 @@
-const path = require('path')
+import { join } from 'path'
 
-const { app, BrowserWindow } = require('electron')
-const isDev = require('electron-is-dev')
+import { app, BrowserWindow } from 'electron'
+import isDev from 'electron-is-dev'
 
 function createWindow() {
   // Create the browser window.
@@ -21,7 +21,7 @@ function createWindow() {
   win.loadURL(
     isDev
       ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
+      : `file://${join(__dirname, '../build/index.html')}`
   )
   // Open the DevTools.
   if (isDev) {
