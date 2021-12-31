d9631804412002cd74599a56941e6b061a7d91ef
Anh Nguyen <anhnguyenquy2407@gmail.com>
2021-12-31 22:11:38 +0700

    1.4.3: New Google Analytics implementation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import ReactGA from 'react-ga'
+import RouteChangeTracker from './RouteChangeTracker'
 import {
   HomePage,
   MotionGenerator,
@@ -9,7 +10,7 @@ import {
   MotionDatabase,
   SubmitNewMotion,
   DebateKeeper,
-  About,
+  About
 } from './pages'
 import { NavBar } from './core/components'
 import { useStyles } from './appStyle'
@@ -17,40 +18,40 @@ import { useStyles } from './appStyle'
 ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID_WEB)
 export default function App() {
   const classes = useStyles()
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search)
-  }, [])
   return (
-    <Router>
-      <div className={classes['App']}>
-        <NavBar />
-        <Switch>
-          <Route exact={true} path='/'>
-            <HomePage />
-          </Route>
-          <Route path='/generator'>
-            <MotionGenerator />
-          </Route>
-          <Route path='/admin'>
-            <AdminPage />
-          </Route>
-          <Route path='/database'>
-            <MotionDatabase />
-          </Route>
-          <Route path='/new_motion'>
-            <SubmitNewMotion />
-          </Route>
-          <Route path='/break_calculator'>
-            <BreakCalculator />
-          </Route>
-          <Route path='/keeper'>
-            <DebateKeeper />
-          </Route>
-          <Route path='/about'>
-            <About />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <>
+      <RouteChangeTracker />
+      <Router>
+        <div className={classes['App']}>
+          <NavBar />
+          <Switch>
+            <Route exact={true} path='/'>
+              <HomePage />
+            </Route>
+            <Route path='/generator'>
+              <MotionGenerator />
+            </Route>
+            <Route path='/admin'>
+              <AdminPage />
+            </Route>
+            <Route path='/database'>
+              <MotionDatabase />
+            </Route>
+            <Route path='/new_motion'>
+              <SubmitNewMotion />
+            </Route>
+            <Route path='/break_calculator'>
+              <BreakCalculator />
+            </Route>
+            <Route path='/keeper'>
+              <DebateKeeper />
+            </Route>
+            <Route path='/about'>
+              <About />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   )
 }
