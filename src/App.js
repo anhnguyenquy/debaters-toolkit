e59ffaa3fb2b0857060177790f93b905ebb6fb29
Anh Nguyen <anhnguyenquy2407@gmail.com>
2021-12-31 21:59:57 +0700

    1.4.2: Still fixing Google Analytics

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ import {
 import { NavBar } from './core/components'
 import { useStyles } from './appStyle'
 
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID_WEB)
 export default function App() {
   const classes = useStyles()
   useEffect(() => {
-    ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID_WEB)
     ReactGA.pageview(window.location.pathname + window.location.search)
   }, [])
   return (
